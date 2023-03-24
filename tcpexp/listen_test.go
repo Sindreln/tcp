12edcf7a31e9a59156d29b3d83f0814d6d0f3bc9
Janis Gailis <janis.gailis@uia.no>
2023-03-24 06:27:41 +0000

    first tcp

---
+++ tcpexp/listen_test.go
@@ -0,0 +1,16 @@
+package tcpexp
+
+import (
+	"net"
+	"testing"
+)
+
+func TestListener(t *testing.T) {
+    	listener, err := net.Listen("tcp", "127.0.0.1:")
+    	if err != nil {
+      		t.Fatal(err)
+    	}
+    	defer func() { _ = listener.Close() }()
+        
+	t.Logf("bundet til %q", listener.Addr())
+  }
