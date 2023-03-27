12a11e58509049c79a2898177895750b97c0800c
Sindreln <112351024+Sindreln@users.noreply.github.com>
2023-03-27 09:57:31 +0200

    la til go mod

--- tcpexp/listen_test.go
+++ tcpexp/listen_test.go
@@ -6,11 +6,11 @@ import (
 )
 
 func TestListener(t *testing.T) {
-    	listener, err := net.Listen("tcp", "127.0.0.1:")
-    	if err != nil {
-      		t.Fatal(err)
-    	}
-    	defer func() { _ = listener.Close() }()
-        
+	listener, err := net.Listen("tcp", "127.0.0.1:")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() { _ = listener.Close() }()
+
 	t.Logf("bundet til %q", listener.Addr())
-  }
+}
